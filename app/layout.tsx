3607b5c24954121f4fe196cc10a2fb24181dab5a
claudio silva <claudiosilva.one@gmail.com>
2024-08-15 14:28:46 -0300

    feat : layout ui

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import { EdgeStoreProvider } from '@/lib/edgestore'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Upload files',
+  description: '🗃️ Upload files to the EdgeStore'
+}
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <EdgeStoreProvider>{children}</EdgeStoreProvider>
+      </body>
+    </html>
+  )
+}
