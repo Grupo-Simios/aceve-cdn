c920f447d41682a9f67b83360d72cf7492544b69
claudio silva <claudiosilva.one@gmail.com>
2024-08-16 06:18:39 -0300

    feat : upload ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@
 
 import { SingleImageDropzone } from '@/components/singleImageDropzone'
 import { useEdgeStore } from '@/lib/edgestore'
-import Link from 'next/link'
+import { Files } from 'lucide-react'
 import { useState } from 'react'
+import { useForm, Controller } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { z } from 'zod'
 
 export default function Page() {
   const [file, setFile] = useState<File>()
@@ -14,8 +18,40 @@ export default function Page() {
   }>()
   const { edgestore } = useEdgeStore()
 
+  const schemaUpload = z.object({
+    name: z.string().min(1, 'Campo Obrigatório'),
+    imgType: z.enum(['padrão', 'logo', 'postagem', 'miniatura', 'icon']),
+    url: z.string().url('URL inválida'),
+    author: z.enum(['dinho', 'davi'])
+  })
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors }
+  } = useForm({
+    resolver: zodResolver(schemaUpload)
+  })
+
+  const onSubmitImg = (data: any) => {
+    console.log(data)
+  }
+
+  const [fieldValues, setFieldValues] = useState({})
+
+  const handleinputValue = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = event.target
+    setFieldValues(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+  console.log(fieldValues)
+
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen gap-8">
       <div className="flex flex-col items-center m-6 gap-2">
         <SingleImageDropzone
           width={256}
@@ -56,16 +92,130 @@ export default function Page() {
         >
           Upload
         </button>
-        {urls?.url && (
-          <Link href={urls.url} target="_blank">
-            URL
-          </Link>
-        )}
-        {urls?.thumbnailUrl && (
-          <Link href={urls.thumbnailUrl} target="_blank">
-            THUMBNAIL
-          </Link>
-        )}
+      </div>
+
+      <div className="w-[30%]">
+        <form
+          onSubmit={handleSubmit(onSubmitImg)}
+          className="flex flex-col gap-2"
+        >
+          <fieldset className="flex flex-col gap-2">
+            <label htmlFor="author" className="text-[12px]">
+              Administrador
+            </label>
+            <Controller
+              name="author"
+              control={control}
+              render={({ field }) => (
+                <select
+                  id="author"
+                  {...field}
+                  className="bg-black text-white"
+                  value={urls?.url}
+                  onChange={(event: React.ChangeEvent<HTMLSelectElement>) =>
+                    handleinputValue(event)
+                  }
+                >
+                  <option value="">Selecione</option>
+                  <option value="Dinho">Dinho</option>
+                  <option value="Davi">Davi</option>
+                </select>
+              )}
+            />
+            {errors.author && (
+              <span className="text-red-500">{errors.author.message}</span>
+            )}
+          </fieldset>
+
+          <fieldset className="flex flex-col gap-2">
+            <label htmlFor="titulo" className="text-[12px]">
+              Titulo da Imagem
+      
+                <input
+                  id="titulo"
+                  type="text"
+                  value={setFieldValues.imgName}
+                  className="bg-black ring-1 ring-slate-100 px-2 py-2 rounded-md"
+                  placeholder="Titulo da imagem"
+                  {...register("titulo")}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                    handleinputValue(event)
+                  }
+                />
+         
+            {errors.imgName && (
+              <span className="text-red-500">{errors.imgName.message}</span>
+            )}
+          </fieldset>
+
+          <fieldset className="flex flex-col gap-2">
+            <label htmlFor="urlimg" className="text-[12px]">
+              Url da Imagem
+            </label>
+            <Controller
+              name="url"
+              control={control}
+              render={({ field }) => (
+                <input
+                  id="urlimg"
+                  type="text"
+                  className="bg-black ring-1 ring-slate-100 px-2 py-2 rounded-md"
+                  placeholder="Url da Imagem"
+                  value={urls?.url} // Valor controlado externamente
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    field.onChange(event.target.value) // Atualiza o valor no react-hook-form
+                    handleinputValue(event) // Controla o valor manualmente
+                  }}
+                />
+              )}
+            />
+            {errors.url && (
+              <span className="text-red-500">{errors.url.message}</span>
+            )}
+          </fieldset>
+
+          <fieldset className="flex flex-col gap-2">
+            <label htmlFor="imgtype" className="text-[12px]">
+              Tipo
+            </label>
+
+            <Controller
+              name="imgType"
+              control={control}
+              value={setFieldValues.imgType}
+              render={({ field }) => (
+                <select
+                  id="imgtype"
+                  {...field}
+                  className="bg-black text-white"
+                  value={setFieldValues.imgType}
+                  onChange={(event: React.ChangeEvent<HTMLSelectElement>) =>
+                    handleinputValue(event)
+                  }
+                >
+                  <option value="">Selecione</option>
+                  <option value="padrão">Padrão</option>
+                  <option value="logo">Logo</option>
+                  <option value="postagem">Postagem</option>
+                  <option value="miniatura">Miniatura</option>
+                  <option value="icon">Icon</option>
+                  <option value="fundo">Fundo</option>
+                </select>
+              )}
+            />
+
+            {errors.imgType && (
+              <span className="text-red-500">{errors.imgType.message}</span>
+            )}
+          </fieldset>
+
+          <button
+            type="submit"
+            className="bg-green-700 text-white rounded-lg py-2 px-2 my-6"
+          >
+            Cadastrar
+          </button>
+        </form>
       </div>
     </div>
   )
