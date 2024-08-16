ab0cd426a0224f6c0a68513ec64dc2182568a75d
claudio silva <claudiosilva.one@gmail.com>
2024-08-16 06:17:17 -0300

    feat : add upload route

---
+++ app/api/send-img/route.ts
@@ -0,0 +1,32 @@
+import dbConnect from '@/app/config/mongodb/mongoconnect'
+import { NewUploadSchema } from '@/app/model/uploadmodel'
+
+export async function POST(req: Request) {
+  try {
+    await dbConnect()
+    const { imgName, imgType, url, author } = await req.json()
+
+    if (!imgName || !imgType || !url || !author) {
+      console.log('Dados Obrigatórios')
+      return new Response(JSON.stringify({ msg: 'Falha ao Obter Dados' }), {
+        status: 400
+      })
+    }
+
+    const newImageUpload = new NewUploadSchema({
+      imgName,
+      imgType,
+      url,
+      author
+    })
+    await newImageUpload.save()
+
+    return Response.json({ msg: 'Imagem Salva Com Sucesso' })
+  } catch (error: any) {
+    console.error('Erro:', error)
+    return Response.json(
+      { msg: 'Falha ao Salvar Imagem', errorMsg: error.message },
+      { status: 500 }
+    )
+  }
+}
