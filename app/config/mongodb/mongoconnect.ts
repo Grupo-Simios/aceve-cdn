20417e43287d6e4927483ac2ec6fc9888ff1b33f
claudio silva <claudiosilva.one@gmail.com>
2024-08-16 14:33:20 -0300

    fix : added advanced error handling and reconnections in mongoose

--- app/config/mongodb/mongoconnect.ts
+++ app/config/mongodb/mongoconnect.ts
@@ -2,32 +2,69 @@ import mongoose from 'mongoose'
 
 const connection: { isConnected?: number } = {}
 
+async function handleReconnect() {
+  console.log('Tentando reconectar ao MongoDB...')
+  try {
+    await mongoose.connect(process.env.MONGODB_URI as string)
+    console.log('Reconexão bem-sucedida!')
+  } catch (error: any) {
+    console.error('Erro ao reconectar ao MongoDB:', error.message)
+  }
+}
+
 async function dbConnect() {
   try {
-    if (!process.env.MONGODB_URI) {
-      console.error('String de Conexão Inválida')
-      return
+    const mongoUri = process.env.MONGODB_URI
+    if (!mongoUri) {
+      throw new Error('String de Conexão Inválida')
     }
 
-    // Verifica se já existe uma conexão ativa
     if (connection.isConnected) {
       console.log('Já está conectado ao MongoDB')
       return
     }
 
-    const connectDb = await mongoose.connect(process.env.MONGODB_URI as string)
-    connection.isConnected = connectDb.connections[0].readyState
+    const options = {
+      connectTimeoutMS: 10000,
+      serverSelectionTimeoutMS: 5000,
+      maxPoolSize: 10,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+
+    const { connection: dbConnection } = await mongoose.connect(
+      mongoUri,
+      options
+    )
+    connection.isConnected = dbConnection.readyState
 
     if (connection.isConnected === 1) {
-      // 1 indica que a conexão está aberta
       console.log('Conexão Estabelecida com Sucesso')
+      if (dbConnection.db) {
+        await dbConnection.db.admin().ping()
+      } else {
+        console.error('Banco de dados não está definido')
+      }
     } else {
       console.error('Conexão Falhou')
     }
-  } catch (error: any) {
-    console.error('Erro ao Conectar ao MongoDB:', error.message)
-    throw new Error(`Falha na Conexão: ${error.message}`)
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error('Erro ao Conectar ao MongoDB:', error.message)
+      handleReconnect()
+      throw new Error(`Falha na Conexão: ${error.message}`)
+    } else {
+      console.error('Erro desconhecido ao conectar ao MongoDB')
+      throw new Error('Falha na Conexão: Erro desconhecido')
+    }
   }
 }
+
+process.on('SIGINT', async () => {
+  console.log('Desconectando do MongoDB...')
+  await mongoose.disconnect()
+  console.log('Desconectado com sucesso!')
+  process.exit(0)
+})
 
 export default dbConnect
